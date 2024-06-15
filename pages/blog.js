94b885b8b083207885bb47836bb1609d30ee6ea0
aryanraj13 <123410989+aryanraj13@users.noreply.github.com>
2024-06-16 02:18:00 +0530

    Created SSR

--- pages/blog.js
+++ pages/blog.js
@@ -5,26 +5,37 @@ import Link from 'next/link'
 //Step 1 : Collect all files from blogdata directory
 //step2 : Iterate through them and display them
 
-const Blog = () => {
-  const [blogs,setBlogs]=useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:3000/api/blogs').then((a)=>{
-      return a.json();})
-      .then((parsed)=>{
-        setBlogs(parsed)
-    })
-  },[])
+const Blog = (props) => {
+  //console.log(props);
+  const [blogs,setBlogs]=useState(props.allBlogs);
+  // useEffect(()=>{
+  //   fetch('http://localhost:3000/api/blogs').then((a)=>{
+  //     return a.json();})
+  //     .then((parsed)=>{
+  //       setBlogs(parsed)
+  //   })
+  // },[])
   return (
     <div className={`${styles.main} ${styles.container}`}>
       {blogs.map((blogitem)=>{
         return <div key ={blogitem.slug} className={styles.blogItem}>
         <Link href={`/blogpost/${blogitem.slug}`}>
         <h3>{blogitem.title}</h3></Link>
-        <p className={styles.description}>{blogitem.content.substr(0,300)}</p>
+        <p className={styles.description}>{blogitem.metadesc.substr(0,300)}</p>
       </div>
       })}
     </div>
   )
 }
+
+export async function getServerSideProps(context) {
+  let data = await fetch('http://localhost:3000/api/blogs')
+  let allBlogs=await data.json()
+      
+  return { 
+    props: { allBlogs }, 
+  }
+}
+
 
 export default Blog
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -4,19 +4,22 @@ import styles from '../../styles/BlogPost.module.css'
 
 //Step1: Find the file corresponding to the slug
 //Step2: Populate through them inside the page
-const slug = () => {
-  const router=useRouter();
-  const [blog,setBlog] = useState();
-    useEffect(()=>{
-      if(!router.isReady) return;
-    const {slug} = router.query;
-      fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a)=>{
-      return a.json();
-    })
-      .then((parsed)=>{
-        setBlog(parsed)
-    })
-    },[router.isReady])   
+const slug = (props) => {
+  function createMarkup(c){
+    return {__html: c};
+  }
+  const [blog,setBlog] = useState(props.myBlog);
+  // const router=useRouter();
+  //   useEffect(()=>{
+  //     if(!router.isReady) return;
+  //   const {slug} = router.query;
+  //     fetch(`http://localhost:3000/api/getblog?slug=${slug}`).then((a)=>{
+  //     return a.json();
+  //   })
+  //     .then((parsed)=>{
+  //       setBlog(parsed)
+  //   })
+  //   },[router.isReady])   
   return (
     <div className={styles.container}>
       <main className={`${styles.main}`}>
@@ -24,12 +27,22 @@ const slug = () => {
       <h1>{blog && blog.title}</h1>
       </div>
       <hr/>
-      <div>
-        {blog && blog.content}
-      </div>
+      {blog && <div dangerouslySetInnerHTML={createMarkup(blog.content)}></div>}
       </main>
     </div>
   )
 }
+
+
+export async function getServerSideProps(context) {
+  
+    const {slug} = context.query;
+    let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+    let myBlog=await data.json();
+      
+  return { 
+    props: { myBlog }, 
+  }
+}    
 
 export default slug
